8efd1c4faa71ffe780e23f61020213f903d6c1ba
Ivan Braiko <ivan.braiko@jetbrains.com>
2023-06-13 17:36:04 +0300

    minor

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,18 +1,14 @@
-﻿//System Specific
+﻿
 global using System;
 global using System.Collections.Generic;
 
-//Maui Specific
 global using Microsoft.Maui;
 global using Microsoft.Maui.Controls;
 global using Microsoft.Maui.Controls.Hosting;
 global using Microsoft.Maui.Hosting;
 
-//Toolkit Specific
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
-
-//App Specific
 global using MauiScientificCalculator.Views;
 global using MauiScientificCalculator.ViewModels;
 
